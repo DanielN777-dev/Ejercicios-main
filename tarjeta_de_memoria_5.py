4c0a13514f75cb4571ae361f6faaf3a1224d76fd
Pioluko <matiasosorio0510@gmail.com>
2026-07-16 18:28:16 -0500

    tarjeta_de_memoria_5

---
+++ tarjeta_de_memoria_5.py
@@ -0,0 +1,127 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+        QApplication, QWidget, 
+        QHBoxLayout, QVBoxLayout, 
+        QGroupBox, QButtonGroup, QRadioButton,  
+        QPushButton, QLabel)
+from random import shuffle
+
+class Question():
+    def __init__(self, question, right_answer, wrong1, wrong2, wrong3):
+        self.question = question
+        self.right_answer = right_answer
+        self.wrong1 = wrong1
+        self.wrong2 = wrong2
+        self.wrong3 = wrong3
+
+app = QApplication([])
+
+btn_OK = QPushButton('Responder')
+lb_Question = QLabel('¡La pregunta más difícil del mundo!')
+
+RadioGroupBox = QGroupBox("Opciones de respuesta")
+rbtn_1 = QRadioButton('Opción 1')
+rbtn_2 = QRadioButton('Opción 2')
+rbtn_3 = QRadioButton('Opción 3')
+rbtn_4 = QRadioButton('Opción 4')
+
+radioGroup = QButtonGroup()
+radioGroup.addButton(rbtn_1)
+radioGroup.addButton(rbtn_2)
+radioGroup.addButton(rbtn_3)
+radioGroup.addButton(rbtn_4)
+
+layout_ans1 = QHBoxLayout()
+layout_ans2 = QVBoxLayout()     
+layout_ans3 = QVBoxLayout()
+layout_ans2.addWidget(rbtn_1)
+
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3)   
+layout_ans3.addWidget(rbtn_4)
+
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3)
+
+RadioGroupBox.setLayout(layout_ans1)
+
+AnsGroupBox = QGroupBox("Resultado de prueba")
+lb_Result = QLabel('¿Es correcto o no?')
+lb_Correct = QLabel('¡Aquí estará la respuesta!')
+
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+
+layout_line1 = QHBoxLayout()
+layout_line2 = QHBoxLayout()   
+layout_line3 = QHBoxLayout()
+
+layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+layout_line2.addWidget(RadioGroupBox)
+layout_line3.addWidget(AnsGroupBox)
+AnsGroupBox.hide()
+
+layout_line3.addStretch(1)
+layout_line3.addWidget(btn_OK, stretch=2)
+layout_line3.addStretch(1)
+
+layout_card = QVBoxLayout()
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line3, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5)
+
+def show_question():
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_OK.setText('Responder')
+    radioGroup.setExclusive(False)
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    radioGroup.setExclusive(True)
+
+answers = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
+
+def ask(q: Question):
+    '''Esta funciòn escribe la pregunta y las respuestas en los widgets'''
+    shuffle(answers)
+    answers[0].setText(q.right_answer)
+    answers[1].setText(q.wrong1)
+    answers[2].setText(q.wrong2)
+    answers[3].setText(q.wrong3)
+    lb_Question.setText(q.question)
+    lb_Correct.setText(q.right_answer)
+    show_question()  
+def show_result():
+    '''Muestra el panel de respuesta'''
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_OK.setText('Siguiente pregunta')
+    
+def show_correct(res):
+    '''Mostrar el resultado - colocar el texto que fue pasado mostrando el panel relevante'''
+    lb_Result.setText(res)
+    show_result()
+    
+def check_answer():
+    '''Si una de las opciones de respuesta es seleccionada, muestra el panel de respuesta '''
+    if answers[0].isChecked():
+        show_correct('¡Correcto!')
+    else:
+        if answers[1].isChecked() or answers[2].isChecked() or answers[3].isChecked():
+            show_correct('Incorrecto!')
+            
+window = QWidget()
+window.setLayout(layout_card)
+window.setWindowTitle('tarjeta de memoria')
+q = Question('Cuál es el planeta más grande del Sistema Solar?', 'Tierra', 'Marte', 'Venus', 'Saturno')
+ask(q)
+btn_OK.clicked.connect(check_answer)
+window.show()
+app.exec_()
